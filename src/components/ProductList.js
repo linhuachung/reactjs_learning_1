113e8e3d6f0aeec1c288ba39608f2966b01ea040
huachung <huachung1304@gmail.com>
2021-04-09 20:43:17 +0700

    new_project

---
+++ src/components/ProductList.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import ProductItem from "./ProductItem";
+class ProductsList extends Component {
+  render() {
+    return (
+      <section id="smartphone" className="container-fluid pt-5 pb-5 bg-dark">
+        <h1 className="text-white text-center">BEST SMARTPHONE</h1>
+        <div className="row">
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
+        </div>
+      </section>
+    );
+  }
+}
+export default ProductsList;
